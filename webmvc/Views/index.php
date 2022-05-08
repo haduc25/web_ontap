8ecf2b76fc4870a0282209f5ea757a5f1bead57c
Ha Duc <haducvcvb@gmail.com>
2022-05-08 18:25:27 +0700

    commit them css cho trang index

--- webmvc/Views/index.php
+++ webmvc/Views/index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Index</title>
+    <title>Trang chủ - Danh sách sản phẩm</title>
     <style>
         h1
         {
@@ -22,13 +22,31 @@
         table, th, td
         {
             border: 1px solid #000;
-            
+            padding: 3px;
         }
+
+        a
+        {
+            text-decoration: none;
+            color: #fff;
+            padding: 2px 8px;
+            margin-left: 5px;
+            margin-right: 5px;
+            background-color: #000;
+            opacity: .8;
+            /* border: 1px solid #000; */
+        }
+
+        a:hover
+        {
+            color: #000;
+            background-color: #fff;
+            text-decoration: underline;
+        }
+
     </style>
 </head>
 <body>
-        <h1>That is index in Views Folder!</h1>
-
         <h2>Danh sách sản phẩm</h2>
         <table>
             <tr>
@@ -36,6 +54,7 @@
                 <th>Tên sản phẩm</th>
                 <th>Mã nhà sản xuất</th>
                 <th>Hình ảnh</th>
+                <th><a href="">Thêm</a></th>
             </tr>
             <?php foreach ($prds as $prd) {
             ?>
@@ -44,6 +63,10 @@
                     <td><?=$prd->ten_sp?></td>
                     <td><?=$prd->ma_nhasx?></td>
                     <td><?=$prd->hinhanh?></td>
+                    <td>
+                        <a href="">Sửa</a>
+                        <a href="">Xóa</a>
+                    </td>
                 </tr>
             <?php } ?>
         </table>
